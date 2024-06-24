38e64e5c28b3541481acfacdc67ffc262d643ea2
AndrewJot <andrewjotjz@gmail.com>
2024-06-24 16:50:12 +1000

    Installed node for NPM. Then, used NPM to initialize backend files, used NPM to install express, nodemon, dotenv and mongoose. Created express app, created middleware, created route handler, and successfully connected to DB (currently JSON). DOTENV file added, GITIGNORE file also added.

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,23 @@
-console.log("test");
+//import modules
+require('dotenv').config()
+const express = require('express');
+
+//create express app
+const app = express();
+
+//create middleware - logs request path and request method
+app.use((req,res,next) => {
+    console.log("Request path:",req.path, '\n', "Request method:", req.method);
+    next();
+})
+
+//route handler
+app.get('/', (req,res) => {
+    const message = "Received json message from localhost " + process.env.PORT;
+    res.json({msg: message})
+});
+
+//Connect to DB - currently using JSON
+app.listen(process.env.PORT, () => {
+    console.log("Connected to DB. Listening to port", process.env.PORT)
+});
