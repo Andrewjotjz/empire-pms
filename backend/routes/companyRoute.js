2e82de34d5589fb3f5ca39e0637c0bbd44a63cb9
AndrewJot <andrewjotjz@gmail.com>
2024-06-28 16:57:55 +1000

    created COMPANY router for API call, COMPANY controller for handling req and res, and middleware to access incoming request with JSON payload, which then converts it into Javascript object, that can be accessed using req.body property

---
+++ backend/routes/companyRoute.js
@@ -0,0 +1,24 @@
+//import modules
+const express = require('express');
+
+//create express' router
+const router = express.Router();
+
+//GET - get all companies
+//? currently not required, perhaps in the future, an admin user can create multiple companies.
+
+//GET - get a single company
+router.get('/:id', getSingleCompany)
+
+//POST - create a new company
+router.post('/create', createNewCompany)
+
+//PUT - update a single company
+router.put('/:id', updateSingleCompany)
+
+//DELETE - delete a single company
+//? Currently the whole system only has one company 'EmpireCBS', perhaps in the future, an admin can create multiple companies and delete the company if needed.
+
+//export the companyRoute's router module
+module.exports = router;
+
--- backend/server.js
+++ backend/server.js
@@ -7,21 +7,21 @@ dotenv.config({ path: '.env.local' });
 
 const express = require('express');
 const mongoose = require('mongoose');
+const companyRoute = require('./routes/companyRoute');
 
 //create express app
 const app = express();
 
-//create middleware - logs request url, request path and request method
+
+//create middleware - Access to 'req' object and logs request url, request path and request method
 app.use((req,res,next) => {
     console.log("Request URL:", req.url, '\n', "Request path:",req.path, '\n', "Request method:", req.method);
     next();
 })
 
 //route handler
-app.get('/', (req,res) => {
-    const message = "Received json message from localhost " + process.env.PORT;
-    res.json({msg: message})
-});
+app.use('/api/company', companyRoute);
+
 
 //Connect to DB - currently using MongoDB
 mongoose.connect(process.env.MONG_URI)
