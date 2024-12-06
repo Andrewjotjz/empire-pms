072f703364336457cdb7ff3f604007760ed75e9f
AndrewJot <andrewjotjz@gmail.com>
2024-12-06 12:29:22 +1100

    Improve NewBudgetForm by adding icon to remove buttons, subcategory automatic pop-out or disabled. Next: Dropdown project and areas. fetch product type units.

--- frontend/src/pages/forms/NewBudgetForm.js
+++ frontend/src/pages/forms/NewBudgetForm.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
 
+import LoadingScreen from "../loaders/LoadingScreen";
+import SessionExpired from "../../components/SessionExpired"
+
 
 const NewBudgetForm = () => {
   const [budget, setBudget] = useState({
@@ -13,22 +16,7 @@ const NewBudgetForm = () => {
           type_total_m2: 0,
           type_rate: 0,
           type_total_amount: 0,
-          category_obj_ref: [
-            {
-              category_id: '',
-              category_total_m2: 0,
-              category_rate: 0,
-              category_total_amount: 0,
-              subcategory_obj_ref: [
-                {
-                  subcategory_id: '',
-                  subcategory_total_m2: 0,
-                  subcategory_rate: 0,
-                  subcategory_total_amount: 0,
-                },
-              ],
-            },
-          ],
+          category_obj_ref: [],
         },
       },
     ],
@@ -51,11 +39,32 @@ const NewBudgetForm = () => {
     setBudget({ ...budget, entries: newEntries });
   };
 
-  const handleCategoryChange = (entryIndex, categoryIndex, field, value) => {
+  const handleCategoryChange = (entryIndex, categoryIndex, field, value, entry, category) => {
     const newEntries = [...budget.entries];
+  
+    // Update the specified field with the new value
     newEntries[entryIndex].product_type_obj_ref.category_obj_ref[categoryIndex][field] = value;
+  
+    if (field === "category_id"){
+    // Reset subcategories for the category
+    const subcategories = productTypeState
+      .find(type => type._id === entry.product_type_obj_ref.type_id)
+      ?.type_categories
+      .find(cat => cat._id === category.category_id)
+      ?.subcategories.map(subcat => ({
+        subcategory_id: subcat._id,
+        subcategory_total_m2: 0,
+        subcategory_rate: 0,
+        subcategory_total_amount: 0,
+      })) || [];
+  
+    newEntries[entryIndex].product_type_obj_ref.category_obj_ref[categoryIndex].subcategory_obj_ref = subcategories;
+    }
+  
+    // Update the budget state
     setBudget({ ...budget, entries: newEntries });
   };
+  
 
   const handleSubcategoryChange = (entryIndex, categoryIndex, subcategoryIndex, field, value) => {
     const newEntries = [...budget.entries];
@@ -75,16 +84,25 @@ const NewBudgetForm = () => {
     setBudget({ ...budget, entries: newEntries });
   };
 
-  const addSubcategory = (entryIndex, categoryIndex) => {
+  const addSubcategory = (entryIndex, categoryIndex, entry, category) => {
     const newEntries = [...budget.entries];
-    newEntries[entryIndex].product_type_obj_ref.category_obj_ref[categoryIndex].subcategory_obj_ref.push({
-      subcategory_id: '',
-      subcategory_total_m2: 0,
-      subcategory_rate: 0,
-      subcategory_total_amount: 0,
-    });
+    
+    productTypeState
+      .find(type => type._id === entry.product_type_obj_ref.type_id)
+      ?.type_categories
+      .find(cat => cat._id === category.category_id)
+      ?.subcategories.forEach((subcat) => {
+        newEntries[entryIndex].product_type_obj_ref.category_obj_ref[categoryIndex].subcategory_obj_ref.push({
+          subcategory_id: subcat._id,
+          subcategory_total_m2: 0,
+          subcategory_rate: 0,
+          subcategory_total_amount: 0,
+        });
+      });
+  
     setBudget({ ...budget, entries: newEntries });
   };
+  
 
   const removeEntry = (entryIndex) => {
     setBudget((prevBudget) => ({
@@ -160,6 +178,16 @@ const NewBudgetForm = () => {
     };
 }, []);
 
+if (isFetchTypeLoading) { return (<LoadingScreen />); }
+
+if (fetchTypeError) {
+    if(fetchTypeError.includes("Session expired") || fetchTypeError.includes("jwt expired") || fetchTypeError.includes("jwt malformed")){
+        return(<div><SessionExpired /></div>)
+    }
+    return (<div>Error: {fetchTypeError}</div>);
+}
+
+
   return (
     <div className="max-w-6xl mx-auto p-6 bg-white rounded-lg shadow-md">
       <h2 className="text-2xl font-bold mb-3">Create Budget</h2>
@@ -212,14 +240,19 @@ const NewBudgetForm = () => {
         <div>
           <h3 className="text-lg font-medium text-gray-700 mb-2">Entries</h3>
           {budget.entries.map((entry, entryIndex) => (
-            <div key={entryIndex} className="border p-6 rounded-md mb-3 bg-gray-50">
-              <button
-                type="button"
-                onClick={() => removeEntry(entryIndex)}
-                className="px-3 py-1 text-sm border border-transparent rounded-md text-white bg-red-500 hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-400"
-              >
-                Remove Entry
-              </button>
+            <div key={entryIndex} className="border px-6 pb-6 pt-1 rounded-md mb-3 bg-gray-50">
+              <div className='w-full flex justify-end'>
+                <button
+                  type="button"
+                      title='Remove whole entry'
+                  onClick={() => removeEntry(entryIndex)}
+                  className="px-3 py-1 text-sm rounded-md text-red-500 hover:bg-red-500 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-400 transition ease-in-out"
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+                  </svg>
+                </button>
+              </div>
               {/* <h4 className="font-medium mb-1 text-lg text-gray-700">Product Type</h4> */}
               <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4 mb-1">
                 <div className="col-span-1 md:col-span-2 lg:col-span-2">
@@ -268,21 +301,26 @@ const NewBudgetForm = () => {
 
               {/* <h5 className="font-medium mb-2 mt-6 text-gray-700">Categories</h5> */}
               {entry.product_type_obj_ref.category_obj_ref.map((category, categoryIndex) => (
-                <div key={categoryIndex} className="border-l-2 border-purple-200 mb-1 mt-4 p-3 bg-purple-100 bg-opacity-50">
-                  <button
-                    type="button"
-                    onClick={() => removeCategory(entryIndex, categoryIndex)}
-                    className="px-3 py-1 text-sm border border-transparent rounded-md text-white bg-red-500 hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-400"
-                  >
-                    Remove Category
-                  </button>
+                <div key={categoryIndex} className="border-l-2 border-purple-200 mb-1 px-3 pb-3 py-1 bg-purple-100 bg-opacity-50">
+                  <div className='w-full flex justify-end'>
+                    <button
+                      type="button"
+                      title='Remove category'
+                      onClick={() => removeCategory(entryIndex, categoryIndex)}
+                      className="px-2 py-1 text-sm rounded-md text-red-500 hover:bg-red-500 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-400 transition ease-in-out"
+                    >
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                      </svg>
+                    </button>
+                  </div>
 
                   <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4 mb-3">
                     <div className="col-span-1 md:col-span-2 lg:col-span-2">
                       <label className="block text-sm font-medium text-gray-700 mb-1">Category</label>
                       <select
                         value={category.category_id}
-                        onChange={(e) => handleCategoryChange(entryIndex, categoryIndex, 'category_id', e.target.value)}
+                        onChange={(e) => handleCategoryChange(entryIndex, categoryIndex, 'category_id', e.target.value, entry, category)}
                         className="w-full rounded-md border-gray-300 shadow-sm focus:border-purple-300 focus:ring focus:ring-purple-200 focus:ring-opacity-50"
                       >
                         <option value="">Select Category</option>
@@ -325,13 +363,6 @@ const NewBudgetForm = () => {
                   {/* <h6 className="font-medium mb-2 mt-4 text-gray-700">Subcategories</h6> */}
                   {category.subcategory_obj_ref.map((subcategory, subcategoryIndex) => (
                     <div key={subcategoryIndex} className="border-l-2 border-pink-100 mb-1 p-2 bg-pink-100 bg-opacity-50">
-                      <button
-                        type="button"
-                        onClick={() => removeSubcategory(entryIndex, categoryIndex, subcategoryIndex)}
-                        className="px-3 py-1 text-sm border border-transparent rounded-md text-white bg-red-500 hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-400"
-                      >
-                        Remove Subcategory
-                      </button>
                       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4 mb-1">
                         <div className="col-span-1 md:col-span-2 lg:col-span-2">
                           <label className="block text-sm font-medium text-gray-700 mb-1">Subcategory</label>
@@ -341,7 +372,6 @@ const NewBudgetForm = () => {
                             className="w-full rounded-md border-gray-300 shadow-sm focus:border-pink-300 focus:ring focus:ring-pink-200 focus:ring-opacity-50"
                           >
                             <option value="">Select Subcategory</option>
-                            {}
                             {productTypeState.find(type => type._id === entry.product_type_obj_ref.type_id)?.type_categories.find(cat => cat._id === category.category_id)?.subcategories.map((subcat) => (
                               <option key={subcat._id} value={subcat._id}>
                                 {subcat.subcategory_name}
@@ -369,20 +399,33 @@ const NewBudgetForm = () => {
                         </div>
                         <div>
                           <label className="block text-sm font-medium text-gray-700 mb-1">Total Amount</label>
-                          <input
-                            type="number"
-                            value={subcategory.subcategory_total_amount}
-                            onChange={(e) => handleSubcategoryChange(entryIndex, categoryIndex, subcategoryIndex, 'subcategory_total_amount', parseFloat(e.target.value))}
-                            className="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                          />
+                          <div className='flex'>
+                            <input
+                              type="number"
+                              value={subcategory.subcategory_total_amount}
+                              onChange={(e) => handleSubcategoryChange(entryIndex, categoryIndex, subcategoryIndex, 'subcategory_total_amount', parseFloat(e.target.value))}
+                              className="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                            />
+                            <button
+                              type="button"
+                              onClick={() => removeSubcategory(entryIndex, categoryIndex, subcategoryIndex)}
+                              className="px-1 py-1 text-sm rounded-md text-red-500 hover:bg-red-500 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-400 transition ease-in-out"
+                            >
+                              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                              </svg>
+                            </button>
+                          </div>
                         </div>
                       </div>
                     </div>
                   ))}
                   <button
                     type="button"
-                    onClick={() => addSubcategory(entryIndex, categoryIndex)}
-                    className="mt-2 px-3 py-1 text-sm border border-transparent rounded-md text-white bg-pink-500 hover:bg-pink-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-400"
+                    onClick={() => addSubcategory(entryIndex, categoryIndex, entry, category)}
+                    disabled={!productTypeState.find(type => type._id === entry.product_type_obj_ref.type_id)?.type_categories.find(cat => cat._id === category.category_id)?.subcategories?.length}                    
+                    hidden={budget.entries[entryIndex].product_type_obj_ref.category_obj_ref[categoryIndex].subcategory_obj_ref?.length}
+                    className="mt-2 px-2 py-1 disabled:bg-gray-400 disabled:cursor-not-allowed text-xs border border-transparent rounded-md text-white bg-pink-500 hover:bg-pink-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-400"
                   >
                     Add Subcategory
                   </button>
@@ -391,7 +434,8 @@ const NewBudgetForm = () => {
               <button
                 type="button"
                 onClick={() => addCategory(entryIndex)}
-                className="mt-4 px-3 py-1 text-sm border border-transparent rounded-md text-white bg-purple-500 hover:bg-purple-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-400"
+                disabled={!productTypeState.find(type => type._id === entry.product_type_obj_ref.type_id)?.type_categories?.length}                
+                className="mt-4 disabled:bg-gray-400 disabled:cursor-not-allowed px-3 py-1 text-sm border border-transparent rounded-md text-white bg-purple-500 hover:bg-purple-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-400"
               >
                 Add Category
               </button>
